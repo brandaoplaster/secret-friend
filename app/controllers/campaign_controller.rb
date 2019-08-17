02faf96ae95c722968381ebc8148f32bbefc2e80
Brandao Plaster <brandaoplaster@gmail.com>
2019-08-17 15:31:45 -0300

    Created the Campaigns controller

---
+++ app/controllers/campaign_controller.rb
@@ -0,0 +1,19 @@
+class CampaignController < ApplicationController
+  def show
+  end
+
+  def index
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def raffle
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  get 'campaign/show'
+
+  get 'campaign/index'
+
+  get 'campaign/create'
+
+  get 'campaign/update'
+
+  get 'campaign/destroy'
+
+  get 'campaign/raffle'
+
   get 'pages/home'
 
   devise_for :users, :controllers => { registrations: 'registrations' }
