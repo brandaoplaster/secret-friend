6b3b3fc9f670a1e33a3c1d54d111f31132408848
Brandao Plaster <brandaoplaster@gmail.com>
2019-08-17 15:32:09 -0300

    Revert "Created the Campaigns controller"
    This reverts commit 02faf96ae95c722968381ebc8148f32bbefc2e80.

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,6 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  get 'campaign/show'
-
-  get 'campaign/index'
-
-  get 'campaign/create'
-
-  get 'campaign/update'
-
-  get 'campaign/destroy'
-
-  get 'campaign/raffle'
-
   get 'pages/home'
 
   devise_for :users, :controllers => { registrations: 'registrations' }
