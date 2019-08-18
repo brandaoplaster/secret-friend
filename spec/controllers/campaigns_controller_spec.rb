91ecd6ea3690fa1911d6efb829b0246839ae8325
Brandao Plaster <brandaoplaster@gmail.com>
2019-08-18 14:28:21 -0300

    Implemented tests for index and show methods

--- spec/controllers/campaigns_controller_spec.rb
+++ spec/controllers/campaigns_controller_spec.rb
@@ -10,18 +10,38 @@ RSpec.describe CampaignsController, type: :controller do
     sign_in @current_user
   end
 
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
-    end
-  end
-
   describe "GET #index" do
     it "returns http success" do
       get :index
       expect(response).to have_http_status(:success)
     end
+  end
+
+  describe "GET #show" do
+
+    context "campaign exists" do
+      it "Returns success" do
+        campaign = create(:campaign, user: @current_user)
+        get :show, params: {id: campaign.id}
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context "User is not the owner of the campaign" do
+      it "Redirects to root" do
+        campaign = create(:campaign)
+        get :show, params: {id: campaign.id}
+        expect(response).to redirect_to('/')
+      end
+    end
+
+    context "campaign don't exists" do
+      it "Redirects to root" do
+        get :show, params: {id: 0}
+        expect(response).to redirect_to('/')
+      end
+    end
+
   end
 
   describe "GET #create" do
