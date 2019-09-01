d3b286dd8e7f1c8284456923202da71a7a146fd2
Brandao Plaster <brandaoplaster@gmail.com>
2019-09-01 16:25:57 -0300

    Included before_action and implemented index method

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,8 +1,14 @@
 class CampaignsController < ApplicationController
+
+  before_action :authenticate_user!
+  before_action :set_campaign, only: [:show, :destroy, :update, :raffle]
+  before_action :is_owner?, only: [:show, :destroy, :update, :raffle]
+
   def show
   end
 
   def index
+    @campaigns = current_user.campaigns
   end
 
   def create
