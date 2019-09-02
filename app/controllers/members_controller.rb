866b91c01db87acf9d9014add05f0287a73b6af5
Brandao Plaster <brandaoplaster@gmail.com>
2019-09-01 22:48:54 -0300

    Implemented priate methods in controller member and included before action

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,4 +1,10 @@
 class MembersController < ApplicationController
+  before_action :authenticate_user!, except: [:opened]
+
+  before_action :set_member, only: [:show, :destroy, :update]
+  before_action :is_owner?, only: [:destroy, :update]
+  before_action :set_member_by_token, only: [:opened]
+
   def create
   end
 
@@ -6,5 +12,27 @@ class MembersController < ApplicationController
   end
 
   def update
+  end
+
+  private
+
+  def member_params
+    params.require(:member).permit(:name, :email, :campaign_id)
+  end
+
+  def set_member
+    @member = Member.find(params[:id])
+  end
+
+  def set_member_by_token
+    @member = Member.find_by!(token: params[:token])
+  end
+
+  def is_owner?
+    unless current_user == @member.campaign.user
+      respond_to do |format|
+        format.json { render json: false, status: :forbidden}
+      end
+    end
   end
 end
